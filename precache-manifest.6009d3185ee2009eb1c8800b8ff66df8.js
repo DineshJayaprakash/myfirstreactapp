6835c57be339a0d3d74b7e2c837319387736e112
DineshJayaprakash <dineshprakash896@gmail.com>
2020-02-25 23:05:06 +0530

    Updates

---
+++ precache-manifest.6009d3185ee2009eb1c8800b8ff66df8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8b1a568db32d00a67fc1db8ee2011a3",
+    "url": "/myfirstreactapp/index.html"
+  },
+  {
+    "revision": "146d5741acfaadbd858a",
+    "url": "/myfirstreactapp/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c48ff87ecd1516b68dfa",
+    "url": "/myfirstreactapp/static/js/2.14c19b93.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/myfirstreactapp/static/js/2.14c19b93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "146d5741acfaadbd858a",
+    "url": "/myfirstreactapp/static/js/main.bfd50fab.chunk.js"
+  },
+  {
+    "revision": "ffff7432fb71d7c27f46",
+    "url": "/myfirstreactapp/static/js/runtime-main.1c5cfc7b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myfirstreactapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
